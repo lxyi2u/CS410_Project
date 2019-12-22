6c0e1afa0b4395435989aa9b7a83ee5642cf4deb
lxyi2u <372799293@qq.com>
2019-12-22 12:35:53 +0800

    update

--- train.py
+++ train.py
@@ -1,19 +1,25 @@
 from model import lstm
 from datautil import DataGenerator
 from keras.callbacks import ModelCheckpoint
+import numpy as np
 INPUT_LENGTH = 50
 INPUT_FEATURE = 136
 BATCH_SIZE = 64
 EPOCHS = 8
+COUNT = 1721577
+WINDOW_LEN = 50
+PREDICT_DAYS = 10
+TRAIN_NUM = int(COUNT*0.7) - WINDOW_LEN - PREDICT_DAYS + 1
+TEST_NUM = int(COUNT*0.3) - WINDOW_LEN - PREDICT_DAYS + 1
 FILEPATH = "./model/lstm.h5"
 
 
 def train():
 
     train_dataset = DataGenerator(
-        './dataset/data.csv', 10, 50, BATCH_SIZE, 'train')
+        './dataset/data.csv', PREDICT_DAYS, WINDOW_LEN, BATCH_SIZE, 'train')
     test_dataset = DataGenerator(
-        './dataset/data.csv', 10, 50, BATCH_SIZE, 'test')
+        './dataset/data.csv', PREDICT_DAYS, WINDOW_LEN, BATCH_SIZE, 'test')
     model = lstm((INPUT_LENGTH, INPUT_FEATURE))
     # model=linear_regression((INTPUT_LENGTH,INPUT_FEATURE))
     model.summary()
@@ -24,8 +30,10 @@ def train():
 
     model.fit_generator(
         train_dataset,
+        steps_per_epoch=np.ceil(TRAIN_NUM / BATCH_SIZE),
         epochs=EPOCHS,
         validation_data=test_dataset,
+        validation_steps=np.ceil(TEST_NUM / BATCH_SIZE),
         shuffle=True,
         verbose=1,
         callbacks=[checkpoint]
